1cf30f251581f6870ff1337e14aef7e8141d3df9
Bogdan Kravtsov <bkboggy@yahoo.com>
2015-06-05 12:17:06 -0700

    Added RoundUp.cs utility class.

---
+++ BK.WPF.Graphs/Utility/RoundUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BK.WPF.Graphs.Utility
+{
+    public static partial class ExtensionMethods
+    {
+        public static int RoundUp(this int num)
+        {
+            var baseNum = num.GetBaseValue();
+
+            if (num > 100 && num % baseNum * 2 < num)
+            {
+                baseNum /= 10;
+            }
+
+            return (int)(Math.Ceiling(num / (double)baseNum) * baseNum);
+        }
+
+        public static int RoundUp(this double num)
+        {
+            var baseNum = num.GetBaseValue();
+
+            if (num > 100 && (int)num % baseNum * 2 < num)
+            {
+                baseNum /= 10;
+            }
+
+            return (int)(Math.Ceiling(num / baseNum) * baseNum);
+        }
+    }
+}
